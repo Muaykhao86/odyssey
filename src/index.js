23c645644484ed56c3b325d318958de9a0fef117
Muaykhao86 <waveflow18@gmail.com>
2019-06-06 14:35:46 +0100

    react router fix

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMid
 ReactDOM.render(
 
     <Provider store={store}>
-    <Router basename="/odyssey">
+    <Router basename="https://Muaykhao86.github.io/odyssey">
     <App/>
     </Router>
     </Provider> ,document.getElementById('root')
