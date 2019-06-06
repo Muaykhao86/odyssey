dc9db6c11702238a42d708f92831b1643f38d548
Muaykhao86 <waveflow18@gmail.com>
2019-06-06 15:41:31 +0100

    update

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMid
 ReactDOM.render(
 
     <Provider store={store}>
-    <Router basename="/odyssey">
+    <Router basename={process.env.PUBLIC_URL}>
     <App/>
     </Router>
     </Provider> ,document.getElementById('root')
